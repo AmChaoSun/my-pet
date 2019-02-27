8f70528df90197fdd46855e54b4fe3c8fd266845
AmChaoSun <leosunchao@gmail.com>
2019-02-27 21:09:56 +1100

    add xunit test project and in-memory-db

---
+++ MyPet.Test/Repositories/UserRepoTest.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Linq;
+using Microsoft.EntityFrameworkCore;
+using MyPet.Data;
+using MyPet.Data.Interfaces;
+using MyPet.Models;
+using MyPet.Models.DTOs;
+using Xunit;
+
+namespace MyPet.Test.Repositories
+{
+    public class UserRepoTest
+    {
+        private readonly IUserRepository userRepository;
+        public UserRepoTest()
+        {
+            var options = new DbContextOptionsBuilder<MyPetContext>()
+                .UseInMemoryDatabase(databaseName: "MyPet")
+                .Options;
+            MyPetContext myPetContextt = new MyPetContext(options);
+            myPetContextt.Database.EnsureDeleted();
+            myPetContextt.Database.EnsureCreated();
+            userRepository = new UserRepository(myPetContextt);
+        }
+
+        [Fact]
+        public void RegularAddUser()
+        {
+            var zak = new User 
+            {
+                UserName = "zak",
+                Email = "123@example.com",
+                Password = "12345678",
+                CreatedOn = DateTime.Parse("Jan 1, 2009")
+            };
+            var savedUser = userRepository.Add(zak);
+            Assert.Equal(1, userRepository.GetAll().Count());
+            Assert.Equal("zak", savedUser.UserName);
+
+        }
+    }
+}
---
+++ MyPet.Test/UnitTest1.cs
@@ -0,0 +1,20 @@
+using System;
+using Xunit;
+
+namespace MyPet.Test
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void PassingTest()
+        {
+            Assert.Equal(2, 2);
+        }
+
+        [Fact]
+        public void FailingTest()
+        {
+            Assert.Equal(2, 3);
+        }
+    }
+}
